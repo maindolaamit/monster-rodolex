7b19c77e7367ff6c9b0603963ff7980e19f2c04a
maindolaamit <maindola.amit@gmail.com>
2022-03-09 18:36:32 +0530

    added fetch logic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import SearchBox from './components/search-box'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const App = () => {
 
   const [searchField, setSearchField] = useState(''); // value, setValue
+  const [monsters, setMonsters] = useState([]);
+  console.log('rendering');
+
+  // call the api to get list of monsters only once when the component is mounted
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/users')
+      .then(response => {
+        setMonsters(response.data)
+        console.log(response.data);
+      });
+  }, []);
 
   const searchHandler = (e) => {
     const searchString = e.target.value.toLowerCase();
